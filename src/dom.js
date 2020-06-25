cb886161404e53354221b39d66b34eade96b88f2
simoxsimo <touili.mohammed@gmail.com>
2020-06-25 17:07:35 +0000

    Implemented Search for weather stats

--- src/dom.js
+++ src/dom.js
@@ -23,9 +23,20 @@ const dom = (() => {
         return data;
     }
 
+    const createSearchButton = () => {
+        selectors.searchButton = document.createElement('button');
+        selectors.searchButton.setAttribute('id', 'search');
+        selectors.searchButton.appendChild(document.createTextNode('test'));
+        selectors.contentSection.prepend(selectors.searchButton);
+   }
+
     const ipLookUpAsyncOperations = (promise) => {
         return promise()
-               .then(data => storeLocationAtHeader(data));
+               .then(data => {
+                    createSearchButton();
+                    return data;
+               })
+               .then(data => storeLocationAtHeader(data))
     }
 
     const createWeatherSection = () => {
@@ -44,7 +55,7 @@ const dom = (() => {
     }
 
     const storeWeathertemp = (data) => {
-        createWeatherTempHeader(Math.round(data.main.temp - 273.15));
+        createWeatherTempHeader(`${Math.round(data.main.temp - 273.15)} ℃`);
         return data;
     }
 
@@ -67,7 +78,7 @@ const dom = (() => {
 
     const createWeatherTempFeelsLike = (data) => {
         selectors.weatherTempFeelsLike = document.createElement('p');
-        selectors.weatherTempFeelsLike.appendChild(document.createTextNode(`Feels Like: ${Math.round(data - 273.15)}`));
+        selectors.weatherTempFeelsLike.appendChild(document.createTextNode(`Feels Like: ${Math.round(data - 273.15)} ℃`));
         selectors.weatherTempOtherStatsDiv.appendChild(selectors.weatherTempFeelsLike);
     }
 
@@ -78,7 +89,7 @@ const dom = (() => {
 
     const createWeatherPressure = (data) => {
         selectors.weatherPressure = document.createElement('p');
-        selectors.weatherPressure.appendChild(document.createTextNode(`Pressure: ${data}`));
+        selectors.weatherPressure.appendChild(document.createTextNode(`Pressure: ${data} hPa(mb)`));
         selectors.weatherTempOtherStatsDiv.appendChild(selectors.weatherPressure);
     }
 
@@ -89,7 +100,7 @@ const dom = (() => {
 
     const createWeatherHumidity = (data) => {
         selectors.weatherHumidity = document.createElement('p');
-        selectors.weatherHumidity.appendChild(document.createTextNode(`Humidity: ${data}`));
+        selectors.weatherHumidity.appendChild(document.createTextNode(`Humidity: ${data}%`));
         selectors.weatherTempOtherStatsDiv.appendChild(selectors.weatherHumidity);
     }
 
@@ -100,7 +111,7 @@ const dom = (() => {
 
     const createWeatherWindSpeed = (data) => {
         selectors.weatherWindSpeed = document.createElement('p');
-        selectors.weatherWindSpeed.appendChild(document.createTextNode(`Wind: ${data}`));
+        selectors.weatherWindSpeed.appendChild(document.createTextNode(`Wind: ${data} m/s`));
         selectors.weatherTempOtherStatsDiv.appendChild(selectors.weatherWindSpeed);
     }
 
@@ -118,17 +129,110 @@ const dom = (() => {
         return data;
     }
 
+    const unixToDate = (unix) => {
+        const dateObj = new Date(unix * 1000);
+        return dateObj.toLocaleString("en-US", {hour: "numeric", minute: "numeric"})
+    }
+
+    const createWeatherSunTimerDiv = () => {
+        selectors.weatherTempSunTimerDiv = document.createElement('div');
+        selectors.weatherTempSunTimerDiv.setAttribute('id', 'sun-stats');
+        selectors.weatherSection.appendChild(selectors.weatherTempSunTimerDiv);
+    }
+
+    const createWeatherSunriseTimer = (data) => {
+        selectors.weatherSunriseTimer = document.createElement('p');
+        selectors.weatherSunriseTimer.setAttribute('id', 'sunrise');
+        selectors.weatherSunriseTimer.appendChild(document.createTextNode(`Sunrise: ${unixToDate(data)}`));
+        selectors.weatherTempSunTimerDiv.appendChild(selectors.weatherSunriseTimer);
+    }
+
+    const createWeatherSunsetTimer = (data) => {
+        selectors.weatherSunsetTimer = document.createElement('p');
+        selectors.weatherSunsetTimer.setAttribute('id', 'sunset');
+        selectors.weatherSunsetTimer.appendChild(document.createTextNode(`Sunset: ${unixToDate(data)}`));
+        selectors.weatherTempSunTimerDiv.appendChild(selectors.weatherSunsetTimer);
+    }
+
+    const storeWeatherSunTime = (data) => {
+        createWeatherSunTimerDiv();
+        createWeatherSunriseTimer(data.sys.sunrise + data.timezone);
+        createWeatherSunsetTimer(data.sys.sunset + data.timezone);
+        return data;
+    }
+
+    const createWeatherDataTime = (data) => {
+        selectors.weatherDataTime = document.createElement('p');
+        selectors.weatherDataTime.setAttribute('id', 'data-time');
+        selectors.weatherDataTime.appendChild(document.createTextNode(`Updated at ${unixToDate(data)}`));
+        selectors.weatherSection.appendChild(selectors.weatherDataTime);
+    }
+
+    const storeWeatherDataTime = (data) => {
+        createWeatherDataTime(data.dt + data.timezone);
+        return data;
+    }
+
     const weatherAsyncOperations = (promise) => {
         return promise
                .then(data => storeWeathertemp(data))
                .then(data => storeWeatherDesc(data))
                .then(data => storeWeatherOtherStats(data))
+               .then(data => storeWeatherSunTime(data))
+               .then(data => storeWeatherDataTime(data))
+    }
+
+    const createSearchField = () => {
+        selectors.searchField = document.createElement('input');
+        selectors.searchField.setAttribute('type', 'text');
+        selectors.contentSection.prepend(selectors.searchField);
+    }
+
+    const createSearchFieldMacro = (e) => {
+        e.preventDefault();
+        if (e.target && e.target.id === 'search' && selectors.searchField === undefined){
+            createSearchField();
+        }
+        else if (e.target && !(e.target.id === 'search' || e.target.tagName === 'INPUT') && selectors.searchField !== undefined){
+            selectors.searchField.remove();
+            selectors.searchField = undefined;
+        }
+    }
+
+    const validateInput = (input) => {
+        if (!input.value) {
+          input.placeholder = 'Please Fill this field';
+          return false;
+        }
+        return true;
+    }
+
+    const refreshPageContent = () => {
+        selectors.weatherSection.textContent = '';
+    }
+
+    const searchLocationMacro = (e) => {
+        e.preventDefault();
+        if (e.keyCode === 13) {
+            if (validateInput(e.target)){
+                refreshPageContent();
+                selectors.locationHeader.textContent = e.target.value[0].toUpperCase() + e.target.value.slice(1);
+                weatherAsyncOperations(weatherApi.apiCall(Promise.resolve({city : e.target.value})));
+                e.target.value = '';
+            }
+        }
+    }
+
+    const searchLocationEvent = () => {
+        document.addEventListener('click', createSearchFieldMacro);
+        document.addEventListener('keyup', searchLocationMacro);
     }
 
     const run = () => {
         createContentSection();
         createWeatherSection();
         weatherAsyncOperations(weatherApi.apiCall(ipLookUpAsyncOperations(ipLookUp.apiCall)));
+        searchLocationEvent();
     };
     return {run};
 })();
